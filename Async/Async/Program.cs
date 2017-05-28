c0d71e1bcbc4ca47814c0b946814f98565b16035
mac <learningportal2016@gmail.com>
2017-05-28 20:25:57 +0530

    Sample

---
+++ Async/Async/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Async
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //LongRunningMethod();
+            //LongRunningParallelMethod();
+           
+
+            Console.ReadLine();
+        }
+
+        public static void LongRunningMethod()
+        {
+            string s = "";
+            for(int i=0;i<1000000;i++)
+            {
+                s = s + "Hello";
+            }
+        }
+
+        
+
+        public static void LongRunningParallelMethod()
+        {
+            string s = "";
+            Parallel.For(0, 1000000, i =>
+            {
+                s = s + "Hello";
+            });
+        }
+    }
+}
